4cad932cbd322509cb58514507964c3ee3224f8d
Andrew Hulsizer <andrew@tastemade.com>
2014-07-22 11:09:50 -0500

    Fix some small bugs
    Fix kerning layout issues
    Fix textColor issue
    Fix race condition coming from a nib

--- YETICharacterLabel/YETICharacterLabel/YETICharacterLabel.m
+++ YETICharacterLabel/YETICharacterLabel/YETICharacterLabel.m
@@ -31,6 +31,28 @@
     [self setup];
 }
 
+- (void)setBounds:(CGRect)bounds
+{
+    self.textContainer.size = bounds.size;
+    super.bounds = bounds;
+}
+
+- (void)setFrame:(CGRect)frame
+{
+    self.textContainer.size = frame.size;
+    super.frame = frame;
+}
+
+- (void)setLineBreakMode:(NSLineBreakMode)lineBreakMode
+{
+    self.textContainer.lineBreakMode = lineBreakMode;
+}
+
+- (void)setNumberOfLines:(NSInteger)numberOfLines
+{
+    self.textContainer.maximumNumberOfLines = numberOfLines;
+}
+
 - (void)setup
 {
     self.characterTextLayers = [[NSMutableArray alloc] init];
@@ -40,7 +62,7 @@
 
 - (void)setupLayoutManager
 {
-    self.textStorage = [[NSTextStorage alloc] initWithAttributedString:self.attributedText];
+    self.textStorage = [[NSTextStorage alloc] init];
     self.layoutManager = [[NSLayoutManager alloc] init];
     self.textContainer = [[NSTextContainer alloc] initWithSize:self.bounds.size];
     
@@ -50,6 +72,9 @@
     
     self.textContainer.maximumNumberOfLines = self.numberOfLines;
     self.textContainer.lineBreakMode = self.lineBreakMode;
+    
+    [self setAttributedText:self.attributedText];
+    [self setSuperToBlank];
 }
 
 #pragma mark - Overrides
@@ -57,7 +82,7 @@
 {
     NSRange wordRange = NSMakeRange(0, text.length);
     NSMutableAttributedString *attributedText = [[NSMutableAttributedString alloc] initWithString:text];
-    [attributedText addAttribute:(NSString *)kCTForegroundColorAttributeName value:(__bridge id)[UIColor whiteColor].CGColor range:wordRange];
+    [attributedText addAttribute:(NSString *)kCTForegroundColorAttributeName value:(__bridge id)self.textColor.CGColor range:wordRange];
     [attributedText addAttribute:(NSString *)kCTFontAttributeName value:self.font range:wordRange];
     
     NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
@@ -74,6 +99,11 @@
     
     [self cleanOutOldCharacterTextLayers];
     [self.textStorage setAttributedString:attributedText];
+}
+
+- (void)setSuperToBlank {
+    NSAttributedString *blank = [[NSAttributedString alloc] initWithString:@""];
+    [super setAttributedText:blank];
 }
 
 #pragma mark - NSLayoutManagerDelegate
@@ -94,11 +124,65 @@
     
     //clean out the text layer
     self.oldCharacterTextLayers = [self.characterTextLayers mutableCopy];
+    [self.characterTextLayers removeAllObjects];
+}
+
+- (void)updateLayoutCharacter
+{
+    if (self.textStorage.string) {
+        NSRange wordRange = NSMakeRange(0, self.textStorage.string.length);
+        CGRect layoutRect = [self.layoutManager usedRectForTextContainer:self.textContainer];
+        
+        NSInteger textLayerIndex = 0;
+        
+        for (NSUInteger glyphIndex = wordRange.location; glyphIndex < wordRange.length+wordRange.location; glyphIndex += 0) {
+            
+            NSRange glyphRange = NSMakeRange(glyphIndex, 1);
+            
+            // Convert those coordinates to the nearest glyph index
+            NSRange characterRange = [self.layoutManager characterRangeForGlyphRange:glyphRange actualGlyphRange:NULL];
+            
+            // Check to see whether the mouse actually lies over the glyph it is nearest to
+            NSTextContainer *textContainer = [self.layoutManager textContainerForGlyphAtIndex:glyphIndex effectiveRange:nil];
+            
+            //Get Bounding Glyph Rect
+            CGRect glyphRect = [self.layoutManager boundingRectForGlyphRange:glyphRange inTextContainer:textContainer];
+            
+            //Get the location
+            CGPoint location = [self.layoutManager locationForGlyphAtIndex:glyphIndex];
+            
+            //Check Kerning and add to previous rect if needed
+            NSRange glyphKerningRange = [self.layoutManager rangeOfNominallySpacedGlyphsContainingIndex:glyphIndex];
+            
+            //Adjust if needed
+            if (glyphIndex == glyphKerningRange.location && textLayerIndex != 0) {
+                CATextLayer *previousLayer = self.characterTextLayers[textLayerIndex-1];
+                CGRect frame = previousLayer.frame;
+                frame.size.width = CGRectGetMaxX(glyphRect)-CGRectGetMinX(frame);
+                previousLayer.frame = frame;
+            }
+            
+            //Adjust the location
+            glyphRect.origin.y += location.y-(CGRectGetHeight(glyphRect)/2)+(CGRectGetMidY(self.bounds))-(CGRectGetHeight(layoutRect)/2);
+            
+            //Create the new text layer
+            CATextLayer *textLayer = self.characterTextLayers[textLayerIndex];
+            textLayer.frame = glyphRect;
+            
+            //Step the number of characters covered
+            glyphIndex += characterRange.length;
+            textLayerIndex++;
+        }
+    }
 }
 
 - (void)calculateTextLayers
 {
-    [self.characterTextLayers removeAllObjects];
+    //If its only an update return
+    if (self.characterTextLayers.count > 0) {
+        [self updateLayoutCharacter];
+        return;
+    }
     
     if (self.textStorage.string) {
         NSRange wordRange = NSMakeRange(0, self.textStorage.string.length);
@@ -125,11 +209,11 @@
             NSRange glyphKerningRange = [self.layoutManager rangeOfNominallySpacedGlyphsContainingIndex:glyphIndex];
             
             //Adjust if needed
-            if (glyphKerningRange.length > 1 && glyphIndex == glyphKerningRange.location) {
+            if (glyphIndex == glyphKerningRange.location) {
                 CATextLayer *previousLayer = [self.characterTextLayers lastObject];
-                CGRect bounds = previousLayer.frame;
-                bounds.size.width += glyphRect.size.width;
-                previousLayer.frame = bounds;
+                CGRect frame = previousLayer.frame;
+                frame.size.width = CGRectGetMaxX(glyphRect)-CGRectGetMinX(frame);
+                previousLayer.frame = frame;
             }
             
             //Adjust the location
@@ -156,7 +240,7 @@
 {
     NSRange wordRange = NSMakeRange(0, self.textStorage.string.length);
     NSMutableAttributedString *attributedText = [[NSMutableAttributedString alloc] initWithString:self.textStorage.string];
-    [attributedText addAttribute:(NSString *)kCTForegroundColorAttributeName value:(__bridge id)[UIColor whiteColor].CGColor range:wordRange];
+    [attributedText addAttribute:(NSString *)kCTForegroundColorAttributeName value:(__bridge id)self.textColor.CGColor range:wordRange];
     [attributedText addAttribute:(NSString *)kCTFontAttributeName value:self.font range:wordRange];
     
     NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
